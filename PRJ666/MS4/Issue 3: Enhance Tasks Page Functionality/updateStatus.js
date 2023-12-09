b7940976da52800e413213d65f761b70496719d1
ardra-surendran <98429984+ardra-suri@users.noreply.github.com>
2023-12-09 15:09:47 -0500

    Add files via upload

---
+++ PRJ666/MS4/Issue 3: Enhance Tasks Page Functionality/updateStatus.js
@@ -0,0 +1,48 @@
+import {createCollectionDB} from "./indexdb";
+
+export function updateTaskData(updatedData, taskId) {
+    const request = createCollectionDB(); // Don't await here
+
+    request.onerror = function (event) {
+        console.error("Error opening database:", event.target.error);
+    };
+
+    request.onsuccess = function (event) {
+        const db = event.target.result;
+
+        // Retrieve the record by ID
+        const transaction = db.transaction("tasks_data", "readwrite");
+        const store = transaction.objectStore("tasks_data");
+        const getRequest = store.get(taskId);
+
+        getRequest.onsuccess = function (event) {
+            // Modify the data
+            const taskToUpdate = event.target.result;
+
+            console.log(taskToUpdate);
+
+            if (taskToUpdate) {
+                taskToUpdate.id = updatedData.id || taskToUpdate.id;
+                taskToUpdate.status = updatedData.status || taskToUpdate.status;
+                taskToUpdate.title = updatedData.title || taskToUpdate.title;
+                taskToUpdate.description = updatedData.description || taskToUpdate.description;
+                taskToUpdate.priority = updatedData.priority || taskToUpdate.priority;
+                
+                const updateRequest = store.put(taskToUpdate);
+
+                updateRequest.onsuccess = function () {
+                    console.log("Task updated successfully");
+                };
+                updateRequest.onerror = function () {
+                    console.error("Error updating task:", event.target.error);
+                };
+            }
+
+        };
+
+        transaction.oncomplete = function () {
+            // Close the transaction and database
+            db.close();
+        };
+    };
+}
